db5a76f585b083b9c172598043d0b62c6d941d92
40829 <408297372@qq.com>
2018-10-31 23:32:27 +0800

    修改了表中的某些错误

--- data_manage/models.py
+++ data_manage/models.py
@@ -1,22 +1,24 @@
 from django.db import models
-from user_management import models as usermodel
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Data(models.Model):
-    id=models.AutoField(primary_key=True)
-    dataname=models.CharField(max_length=50,null=False)#资料名字
-    comment=models.FileField(upload_to="filed/", verbose_name="文件")
-    createtime=models.DateTimeField(auto_now_add=True)
-    updatetime=models.DateTimeField(auto_now_add=True)
-    user=models.ForeignKey(to=usermodel.User,to_field=id)
+    id = models.AutoField(primary_key=True)
+    data_name = models.CharField(max_length=50, null=False)  # 资料名字
+    comment = models.FileField(upload_to="filed/", verbose_name="文件")
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=User)
 
     def __str__(self):
-        return self.dataname
+        return self.data_name
+
 
 class Category(models.Model):
-    nid=models.AutoField(primary_key=True)
-    title=models.CharField(max_length=30)#分类标题
-    data=models.ForeignKey(to='Data',to_field='id')
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)  # 分类标题
+    data = models.ForeignKey(to='Data', to_field='id')
 
     def __str__(self):
         return self.title
