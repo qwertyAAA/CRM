c34265c4074ef1e868cff28eb36b3e678ee24ef8
czw <czw@hxak.com>
2018-10-31 19:19:41 +0800

    员工信息表，部门信息表

--- user_management/models.py
+++ user_management/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 
@@ -39,5 +41,17 @@ class PermissionInfo(models.Model):
     select = models.BooleanField(default=True, verbose_name="查看权限")
 
 
+# 员工信息表
+class Staff(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_salary = models.FloatField(max_length=32, verbose_name="员工薪资")
+    staff_state = models.BooleanField(default=True, verbose_name="员工状态")
+    info = models.ManyToManyField(to="UserInfo", to_field="id", verbose_name="员工信息id")
 
 
+# 部门信息表
+class Department(models.Model):
+    id = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=32, verbose_name="部门名称")
+    leader_title = models.CharField(max_length=32, verbose_name="领导职称")
+    info = models.ManyToManyField(to="Staff", to_field="id", verbose_name="部门信息id")
