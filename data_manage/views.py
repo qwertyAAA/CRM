32467354caf6facee92532017650e17881911dc4
AllenLeepy <1424749544@qq.com>
2018-11-01 12:44:52 +0800

    实现了查看资料功能

--- data_manage/views.py
+++ data_manage/views.py
@@ -8,7 +8,14 @@ def check(request):
     datalist=models.Data.objects.all()
     head=apps.get_model('data_manage','Data')
     headobj=head._meta.fields
+    headname=[]
+    for i in headobj:
+        headname.append(i.verbose_name)
+    print(headname)
     # print(headname.verbose_name)
     if request.method=='POST':
         pass
-    return render(request,'data_manage/check_data.html',{'datalist':datalist})
+    return render(request,'data_manage/check_data.html',{'datalist':datalist,'headname':headname})
+
+def add(request):
+    pass
