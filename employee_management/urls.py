f711fca55cf3ef598cecfbde9571676877e50303
czw <czw@hxak.com>
2018-11-06 16:09:13 +0800

    czx

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import url
 from employee_management import views
 
 urlpatterns = [
+     url(r"^main/", views.main),
+     url(r"^employee_info/", views.employee_info),
+     url(r"^edit_employee/", views.edit_employee),
+     url(r"^add_employee/", views.add_employee),
+     url(r"^delete_employee/", views.add_employee),
 ]
